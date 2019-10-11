d41de9da177982ec891a24c5437f01b0be25174b
Colin <viewtifuljho@hotmail.com>
2019-10-10 22:08:04 -0500

    pseudocode before submission

--- models/Article.js
+++ models/Article.js
@@ -1,11 +1,8 @@
 var mongoose = require("mongoose");
 var assert = require("assert");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   title: {
     type: String,
@@ -20,16 +17,12 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
   }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
 Article.on("index", function(err) {
@@ -39,5 +32,4 @@ Article.on("index", function(err) {
   })
 })
 
-// Export the Article model
 module.exports = Article;
--- public/app.js
+++ public/app.js
@@ -1,13 +1,12 @@
-// Grab the articles as a json
+
 $.getJSON("/articles", function (data) {
-  // For each one
   for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
     $("#articles").append(
       "<div class='card' data-id='" + data[i]._id + "' style ='width: 36rem;'><div class='card-body'><h5 class=' border-bottom card-title'>" + data[i].title + "</h5><p>" + data[i].story + "</p> <a href='http://www.destructoid.com/" + data[i].link + "' target='_blank'>Full Article</a></div>");
   }
 });
 
+//Creates the comment section on the right-hand part of the window//
 
 $(document).on("click", "div .card", function () {
   $("#notes").empty();
@@ -23,34 +22,29 @@ $(document).on("click", "div .card", function () {
       $("#notes").append("<h4>Leave a comment<h4>")
       $("#notes").append("<form class='border-bottom p-2'><div class='form-group'><label for='name-input'>Name</label><br><input class='w-75 p-1' id='name-input' name='title' ></input></div><div class='form-group'><label for='body-input'>Comment</label><br><textarea class='w-75 p-1' id='body-input' style='resize:none' name='body'></textarea></div><button class='btn btn-secondary' data-id='" + data._id + "' id='savenote'>Submit</button></form><br><h3>Comments</h3>");
 
+      //Need to append each note associated with the article below the comment entry fields//
     });
 });
 
-// When you click the savenote button
+
 $(document).on("click", "#savenote", function () {
-  // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      // Value taken from title input
       name: $("#name-input").val(),
-      // Value taken from note textarea
       body: $("#body-input").val()
     }
   })
-    // With that done
     .then(function (data) {
-      // Log the response
       console.log(data);
-      // Empty the notes section
       $("#notes").empty();
     });
 
-  // Also, remove the values entered in the input and textarea for note entry
   $("#name-input").val("");
   $("#body-input").val("");
 });
+
+//Need to make a click event handler to call the DELETE route on a given note when its delete button is clicked//
